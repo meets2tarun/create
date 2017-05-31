e96cf0c80bef24abf15d840cd29494469a9b775d
meets2tarun <meets2tarun@gmail.com>
2017-05-31 08:59:36 +0530

    edit update-status

--- javascript/update-status.js
+++ javascript/update-status.js
@@ -36,6 +36,7 @@ function update_progress(progress) {
     if (progress.download) {
       download_button.textContent = "CLICK TO DOWNLOAD";
       download_link.href = build_server + progress.download;
+      download_button.textContent = "HOW TO BURN";
       window.location = "http://sucode.me/test.html";
     } else {
       download_button.textContent = "ERROR: iso creation failed";
